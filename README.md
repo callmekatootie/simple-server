c5eb605dd4d06d1ea865ca90c281ab22ec9ee295
Mithun Kamath <callmekatootie@gmail.com>
2014-07-29 15:05:31 +0530

    Initial code with basic server and options to specify port and address

---
+++ README.md
@@ -0,0 +1,4 @@
+simple-server
+=============
+
+A simple command line http-server built using Go.
---
+++ http-server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "net/http"
+)
+
+type serverConfig struct {
+    port *int
+    address *string
+}
+
+func (o *serverConfig) parseFlags() {
+    o.port = flag.Int("p", 8080, "port to listen for connections")
+    o.address = flag.String("a", "127.0.0.1", "address to bind to")
+
+    flag.Parse()
+}
+
+func main() {
+    options := serverConfig{}
+
+    options.parseFlags()
+
+    http.Handle("/", http.FileServer(http.Dir("./")))
+
+    fmt.Printf("Starting http server at %s on port %d \n", *options.address, *options.port)
+
+    fmt.Println("Hit CTRL-C to stop the server")
+
+    address := fmt.Sprintf("%s:%d", *options.address, *options.port)
+
+    err := http.ListenAndServe(address, nil)
+
+    if err != nil {
+        fmt.Println()
+        fmt.Println("ERROR")
+        fmt.Println(err);
+    }
+}
